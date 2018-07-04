499f2d837cff27c6bd25d44d7b94679dd62667dd
666lvchong <36066106+666lvchong@users.noreply.github.com>
2018-07-04 18:17:01 +0800

    lv

--- src/main/java/com/pdkj/jack_shop/model/Shop.java
+++ src/main/java/com/pdkj/jack_shop/model/Shop.java
@@ -82,6 +82,17 @@ public class Shop {
      */
     private String detail_imgs;
 
+
+    private String county;
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
     /**
      * @return id
      */
