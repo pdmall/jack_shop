ceab2e375ae69952cdef6db00117aabe0bcf95f1
666lvchong <36066106+666lvchong@users.noreply.github.com>
2018-07-23 14:17:11 +0800

    lv

--- src/main/java/com/pdkj/jack_shop/dao/UserOrderDao.java
+++ src/main/java/com/pdkj/jack_shop/dao/UserOrderDao.java
@@ -17,7 +17,6 @@ import com.pdkj.jack_shop.util.sql.SQLTools;
 import com.pdkj.jack_shop.util.sql.SqlInfo;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -33,25 +32,28 @@ public class UserOrderDao extends DaoBase<Banner> {
     //添加订单
     public Long addOrder(UserOrder userOrder) {
         userOrder.setId(Tools.generatorId());
-        SqlInfo sqlInfo = SQLTools.getInsertSQL(userOrder,"user_order");
-         jdbcTemplate.update(sqlInfo.getSql(),sqlInfo.getValues());
+        SqlInfo sqlInfo = SQLTools.getInsertSQL(userOrder, "user_order");
+        jdbcTemplate.update(sqlInfo.getSql(), sqlInfo.getValues());
         return userOrder.getId();
     }
+
     //添加订单详情
     public Long addOrderDetails(UserOrderDetails userOrderDetails) {
         userOrderDetails.setId(Tools.generatorId());
-        SqlInfo sqlInfo = SQLTools.getInsertSQL(userOrderDetails,"user_order_details");
-        jdbcTemplate.update(sqlInfo.getSql(),sqlInfo.getValues());
+        SqlInfo sqlInfo = SQLTools.getInsertSQL(userOrderDetails, "user_order_details");
+        jdbcTemplate.update(sqlInfo.getSql(), sqlInfo.getValues());
         return userOrderDetails.getId();
     }
+
     //修改订单
-    public Long updateOrder(UserOrder userOrder){
-        SqlInfo insertSQL =SQLTools.getUpdateById(userOrder,"user_order",userOrder.getId());
-        jdbcTemplate.update(insertSQL.getSql(),insertSQL.getValues());
+    public Long updateOrder(UserOrder userOrder) {
+        SqlInfo insertSQL = SQLTools.getUpdateById(userOrder, "user_order", userOrder.getId());
+        jdbcTemplate.update(insertSQL.getSql(), insertSQL.getValues());
         return userOrder.getId();
     }
+
     //查看订单列表
-    public List<Map<String, Object>> getShopOrder(Long shop_id,Pager page){
+    public List<Map<String, Object>> getShopOrder(Long shop_id, Pager page) {
         MySql sql = new MySql();
         sql.append("SELECT");
         sql.append("	uo.id,");
@@ -65,35 +67,42 @@ public class UserOrderDao extends DaoBase<Banner> {
         sql.append("WHERE ");
         sql.append("	uo.order_state_id = os.id AND");
         sql.append("	uo.id = uod.user_order_id AND");
-        sql.append("	uo.user_id = 6");
+        sql.append("	uo.user_id = 6 ");
         sql.append("GROUP BY uo.id");
         sql.limit(page);
-        return jdbcTemplate.queryForList(sql.toString(),sql.getValues());
+        return jdbcTemplate.queryForList(sql.toString(), sql.getValues());
     }
 
-    public List<Map<String, Object>> getUserOrder(Long user_id,Pager page){
+    public List<Map<String, Object>> getUserOrder(Long user_id, Integer order_state_id,Pager page) {
         MySql sql = new MySql();
-        sql.append("select id,pay_on,item_id,state,pay_type,user_id,created,buy_time,");
-        sql.append("use_time,use_coupon_id,wallet_money,total_price,final_price,shop_id ");
-        sql.append(" FROM user_order ");
-        sql.append(" where user_id = ?",user_id);
+        sql.append("SELECT");
+        sql.append("uo.created,uo.final_price,uod.price,uod.quantity,uo.Pay_time,");
+        sql.append("s.shop_name,s.shop_address,s.home_img,uo.order_state_id");
+        sql.append("FROM");
+        sql.append("user_order AS uo ,user_order_details AS uod ,shop AS s");
+        sql.append("WHERE");
+        sql.append("uo.id = uod.user_order_id AND s.id = uo.shop_id AND");
+        sql.append("uo.user_id  = ? ",user_id);
+        if(order_state_id != 0){
+            sql.append("AND uo.order_state_id  = ? ",order_state_id);
+        }
         sql.limit(page);
-        return jdbcTemplate.queryForList(sql.toString(),sql.getValues());
+        return jdbcTemplate.queryForList(sql.toString(), sql.getValues());
     }
 
     public String getOrderPrice(String order_id) {
         String sql = "select final_price from user_order where id =  ? and order_state_id = 1";
         List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql, order_id);
-        if(maps.size()>0){
+        if (maps.size() > 0) {
             return maps.get(0).get("final_price").toString();
         }
         return null;
     }
 
-    public void paySuccess(String orderId,String pay_time,String trade_type) {
+    public void paySuccess(String orderId, String pay_time, String trade_type) {
         MySql sql = new MySql();
-        sql.append("update user_order set order_state_id = 2 ,pay_time = ? ,trade_type = ?  where id = ? and order_state_id = 1",orderId,pay_time,trade_type);
-        jdbcTemplate.update(sql.toString(),sql.getValues());
+        sql.append("update user_order set order_state_id = 2 ,pay_time = ? ,trade_type = ?  where id = ? and order_state_id = 1", orderId, pay_time, trade_type);
+        jdbcTemplate.update(sql.toString(), sql.getValues());
     }
 
 }
